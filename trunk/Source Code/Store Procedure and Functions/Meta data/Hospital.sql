cfacd418dacd46cc60a32d99562acc4a095ca588
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-01 06:51:09 +0000

    Add script for hospital

---
+++ trunk/Source Code/Store Procedure and Functions/Meta data/Hospital.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO Hospital
+	(
+		Hospital_Name,
+		Hospital_Type,
+		[Address],
+		Ward_ID,
+		District_ID,
+		City_ID,
+		Phone_Number,
+		Fax,
+		Email,
+		Website,
+		Coordinate,
+		Created_Person,
+		Is_Active
+	)
+VALUES
+	(N'SOS International', 7, N'167A Nam Kỳ Khởi Nghĩa, Quận 3, Tp.Hồ Chí Minh',
+	'', 770, 79, '(08) 38298520', '(08) 38298551', 'www.internationalsos.com',
+	'', '10.784075, 106.689859', 1, 'True')
