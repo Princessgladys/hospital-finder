59f542a500a1bc5f1c3b99e17aee6b59a987ef83
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-03 06:31:31 +0000

    Add function FU_AUTO_GENERATE_ENTITIES_CLASS and FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE in database

---
+++ Source Code/Store Procedure and Functions/Functions/FU_AUTO_GENERATE_ENTITIES_CLASS.sql
@@ -0,0 +1,117 @@
+﻿-- FUNCTION TO GENERATE C# ENTITIES CLASS
+-- SONNX
+CREATE FUNCTION [dbo].[FU_AUTO_GENERATE_ENTITIES_CLASS]
+(
+	@TableName VARCHAR(MAX),
+	@NameSpace VARCHAR(MAX)
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @result VARCHAR(MAX)
+
+	SET @result = 'using System;' + CHAR(13) + CHAR(13)
+
+	SET @result = @result + 'namespace ' +
+		@NameSpace  + CHAR(13) + '{' + CHAR(13)
+
+	SET @result = @result + 
+		'	/// <summary>' + CHAR(13) +
+		'	/// Class defines properties for ' + @TableName + ' table' + CHAR(13) +
+		'	/// <summary>' + CHAR(13) +
+		'	public class ' +
+		REPLACE(@TableName, '_', '') + CHAR(13) + '	{' + CHAR(13)
+
+	SET @result = @result + '		#region ' + @TableName + ' Properties' + CHAR(13) 
+
+	SELECT @result = @result + CHAR(13) +
+		   '		/// <summary>' + CHAR(13) +
+		   '		/// Property for ' + orgColName + ' attribute' + CHAR(13) +
+		   '		/// <summary>' + CHAR(13) +
+		   '		public ' +
+		   ColumnType + ' ' + ColumnName + ';' + CHAR(13)
+	FROM
+	(
+		SELECT REPLACE(col.name, '_', '') ColumnName,
+			   col.name orgColName, column_id,
+			CASE typ.name 
+				WHEN 'bigint' THEN
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'long?' ELSE 'long' END
+				WHEN 'binary' THEN 'byte[]'
+				WHEN 'bit' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'bool?' ELSE 'bool' END            
+				WHEN 'char' THEN 'string'
+				WHEN 'date' THEN
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'DateTime?' ELSE 'DateTime' END                        
+				WHEN 'datetime' THEN
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'DateTime?' ELSE 'DateTime' END                        
+				WHEN 'datetime2' THEN  
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'DateTime?' ELSE 'DateTime' END                        
+				WHEN 'datetimeoffset' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'DateTimeOffset?' ELSE 'DateTimeOffset' END                                    
+				WHEN 'decimal' THEN  
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'decimal?' ELSE 'decimal' END                                    
+				WHEN 'float' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'float?' ELSE 'float' END                                    
+				WHEN 'image' THEN 'byte[]'
+				WHEN 'int' THEN  
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'int?' ELSE 'int' END
+				WHEN 'money' THEN
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'decimal?' ELSE 'decimal' END                                                
+				WHEN 'nchar' THEN 'string'
+				WHEN 'ntext' THEN 'string'
+				WHEN 'numeric' THEN
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'decimal?' ELSE 'decimal' END                                                            
+				WHEN 'nvarchar' THEN 'string'
+				WHEN 'real' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'double?' ELSE 'double' END                                                                        
+				WHEN 'smalldatetime' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'DateTime?' ELSE 'DateTime' END                                    
+				WHEN 'smallint' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'int?' ELSE 'int'END            
+				WHEN 'smallmoney' THEN  
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'decimal?' ELSE 'decimal' END                                                                        
+				WHEN 'text' THEN 'string'
+				WHEN 'time' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'TimeSpan?' ELSE 'TimeSpan' END                                                                                    
+				WHEN 'timestamp' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'DateTime?' ELSE 'DateTime' END                                    
+				WHEN 'tinyint' THEN 
+					CASE col.IS_NULLABLE
+						WHEN 'TRUE' THEN 'byte?' ELSE 'byte' END                                                
+				WHEN 'uniqueidentifier' THEN 'Guid'
+				WHEN 'varbinary' THEN 'byte[]'
+				WHEN 'varchar' THEN 'string'
+				ELSE 'Object'
+			END ColumnType
+		FROM sys.columns col join
+			 sys.types typ
+		ON col.system_type_id = typ.system_type_id AND
+		   col.user_type_id = typ.user_type_id
+		WHERE object_id = object_id(@TableName)
+	) t
+	ORDER BY column_id
+
+	SET @result = @result + CHAR(13) + '		#endregion '+ CHAR(13) 
+
+	SET @result = @result  + '	}' + CHAR(13) + '}'
+
+	RETURN @result
+END
---
+++ Source Code/Store Procedure and Functions/Functions/FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE.sql
@@ -0,0 +1,59 @@
+﻿-- SCRIPT TO TRANSFORM DIACRITIC VIETNAMESE TO NON-DIACRITIC
+-- SONNX
+CREATE FUNCTION [dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE]
+(
+      @strInput NVARCHAR(4000)
+)
+RETURNS NVARCHAR(4000)
+AS
+BEGIN    
+    IF @strInput IS NULL
+		RETURN @strInput
+    IF @strInput = ''
+		RETURN @strInput
+
+    DECLARE @RT NVARCHAR(4000)
+    DECLARE @DIACRITIC_CHARS NCHAR(136)
+    DECLARE @NON_DIACRITIC_CHARS NCHAR (136)
+ 
+    SET @DIACRITIC_CHARS = N'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệế
+						   ìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵý
+						   ĂÂĐÊÔƠƯÀẢÃẠÁẰẲẴẶẮẦẨẪẬẤÈẺẼẸÉỀỂỄỆẾÌỈĨỊÍ
+						   ÒỎÕỌÓỒỔỖỘỐỜỞỠỢỚÙỦŨỤÚỪỬỮỰỨỲỶỸỴÝ' +
+						   NCHAR(272) + NCHAR(208)
+
+    SET @NON_DIACRITIC_CHARS = N'aadeoouaaaaaaaaaaaaaaaeeeeeeeeee
+							   iiiiiooooooooooooooouuuuuuuuuuyyyyy
+							   AADEOOUAAAAAAAAAAAAAAAEEEEEEEEEEIIIII
+							   OOOOOOOOOOOOOOOUUUUUUUUUUYYYYYDD'
+ 
+    DECLARE @COUNTER INT
+    DECLARE @COUNTER1 INT
+    SET @COUNTER = 1
+ 
+    WHILE (@COUNTER <= LEN(@strInput))
+    BEGIN  
+		SET @COUNTER1 = 1
+		WHILE (@COUNTER1 <= LEN(@DIACRITIC_CHARS)+1)
+		BEGIN
+			IF UNICODE(SUBSTRING(@DIACRITIC_CHARS, @COUNTER1, 1)) =
+			   UNICODE(SUBSTRING(@strInput,@COUNTER ,1))
+				BEGIN          
+					IF @COUNTER = 1
+						SET @strInput = SUBSTRING(@NON_DIACRITIC_CHARS, @COUNTER1, 1) +
+										SUBSTRING(@strInput, @COUNTER + 1,LEN(@strInput) - 1)                  
+					ELSE
+						SET @strInput = SUBSTRING(@strInput, 1, @COUNTER - 1) +
+										SUBSTRING(@NON_DIACRITIC_CHARS, @COUNTER1,1) +
+										SUBSTRING(@strInput, @COUNTER + 1, LEN(@strInput) - @COUNTER)
+						BREAK
+				END
+			SET @COUNTER1 = @COUNTER1 + 1
+		END
+	SET @COUNTER = @COUNTER + 1
+    END
+
+    SET @strInput = REPLACE(@strInput, ' ', ' ')
+
+    RETURN @strInput
+END
