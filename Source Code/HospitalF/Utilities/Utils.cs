275432c589b4642918b6de42df23afa11e70a18a
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-04 18:23:24 +0000

    Add missing HomeView. Add RemoveSpecialCharacter method

--- Source Code/HospitalF/Utilities/Utils.cs
+++ Source Code/HospitalF/Utilities/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace HospitalF.Utilities
@@ -10,5 +11,26 @@ namespace HospitalF.Utilities
     /// </summary>
     public class Utils
     {
+        /// <summary>
+        /// Remove special character in a string and replace with white space
+        /// ',' '.' ';' ''' '"' '[' ']' '{' '}' '\' '/' '<' '>' '-' '_' '+' '=' '*'
+        /// </summary>
+        /// <param name="inputStr">Input string</param>
+        /// <returns>String that does not contain special character</returns>
+        public static string RemoveSpecialCharacter(string inputStr)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char character in inputStr)
+            {
+                if ((character >= '0' && character <= '9') ||
+                    (character >= 'A' && character <= 'Z') ||
+                    (character >= 'a' && character <= 'z') ||
+                    character == ' ')
+                {
+                    sb.Append(character);
+                }
+            }
+            return sb.ToString();
+        }
     }
 }
