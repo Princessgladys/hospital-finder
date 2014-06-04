5b8e5f9c36731c9b1b83ada1fa5deaf949606e33
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-04 18:46:18 +0000

    Remove method RemoveSpecialCharacter

--- trunk/Source Code/HospitalF/Utilities/Utils.cs
+++ trunk/Source Code/HospitalF/Utilities/Utils.cs
@@ -11,26 +11,6 @@ namespace HospitalF.Utilities
     /// </summary>
     public class Utils
     {
-        /// <summary>
-        /// Remove special character in a string and replace with white space
-        /// ',' '.' ';' ''' '"' '[' ']' '{' '}' '\' '/' '<' '>' '-' '_' '+' '=' '*'
-        /// </summary>
-        /// <param name="inputStr">Input string</param>
-        /// <returns>String that does not contain special character</returns>
-        public static string RemoveSpecialCharacter(string inputStr)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (char character in inputStr)
-            {
-                if ((character >= '0' && character <= '9') ||
-                    (character >= 'A' && character <= 'Z') ||
-                    (character >= 'a' && character <= 'z') ||
-                    character == ' ')
-                {
-                    sb.Append(character);
-                }
-            }
-            return sb.ToString();
-        }
+        
     }
 }
