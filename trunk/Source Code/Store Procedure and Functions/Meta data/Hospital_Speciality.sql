5946230c107079032c6d765be75112a6df9c4853
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-05 04:33:49 +0000

    modify Speciality.sql add Hospital_Speciality.sql

--- trunk/Source Code/Store Procedure and Functions/Meta data/Hospital_Speciality.sql
+++ trunk/Source Code/Store Procedure and Functions/Meta data/Hospital_Speciality.sql
@@ -2,7 +2,33 @@
 
 INSERT INTO Hospital_Speciality VALUES
 	-- SOS International
-	(1, 1, 'False', 'True')
+	(15, 5, 'False', 'True'),
+	(15, 10, 'False', 'True'),
+	(15, 12, 'False', 'True'),
+	(15, 15, 'False', 'True'),
+	(15, 16, 'False', 'True'),
+	(15, 23, 'False', 'True'),
+	(15, 25, 'False', 'True'),
+	(15, 27, 'False', 'True'),
+	(15, 28, 'False', 'True'),
+	(15, 29, 'False', 'True'),
+	(15, 34, 'False', 'True'),
+	(15, 36, 'False', 'True'),
+	(15, 39, 'False', 'True'),
+	(15, 41, 'False', 'True'),
+	(15, 42, 'False', 'True'),
+	(15, 44, 'False', 'True'),
+	(15, 49, 'False', 'True'),
+	(26, 5, 'False', 'True'),
+	(26, 16, 'False', 'True'),
+	(26, 20, 'False', 'True'),
+	(26, 27, 'False', 'True'),
+	(26, 30, 'False', 'True'),
+	(26, 35, 'False', 'True'),
+	(26, 36, 'False', 'True'),
+	(26, 40, 'False', 'True'),
+	(26, 43, 'False', 'True'),
+	(41, 40, 'False', 'True'),
 	
 	-- HCM Family Medical Practice
 	
