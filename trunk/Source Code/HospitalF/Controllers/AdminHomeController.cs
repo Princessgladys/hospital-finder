a4a4f735c09939748656388ab1be6eccbdd32a0c
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-17 09:17:58 +0000

    Add layout for source code

---
+++ trunk/Source Code/HospitalF/Controllers/AdminHomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using HospitalF.Constant;
+using HospitalF.Models;
+using HospitalF.Entities;
+using HospitalF.App_Start;
+
+namespace HospitalF.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        /// <summary>
+        /// GET: /AdminHome/Index
+        /// </summary>
+        /// <returns>Task[ActionResult]</returns>
+        [LayoutInjecter(Constants.AdmidLayout)]
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+
+    }
+}
