dd49b9b0310026aa22f58504985ff2da574c2a65
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-19 15:09:57 +0000

    remove DoctorUtil.cs

--- trunk/Source Code/HospitalF/Models/AppointmentModels.cs
+++ trunk/Source Code/HospitalF/Models/AppointmentModels.cs
@@ -83,8 +83,34 @@ namespace HospitalF.Models
 
         #endregion
 
-        #region Insert Appointment into database
-
+        #region Load doctor
+        /// <summary>
+        /// Load all doctor in Doctor_Speciality
+        /// </summary>
+        /// <param name="SpecialityID"></param>
+        /// <returns>List[DoctorEnity] that contains list of doctor with appropriate Speciality code</returns>
+        public static async Task<List<DoctorEntity>> LoadDoctorInDoctorSpecialityAsyn(int SpecialityID)
+        {
+            List<DoctorEntity> doctorList = new List<DoctorEntity>();
+            DoctorEntity doctor = null;
+            List<SP_LOAD_DOCTOR_IN_DOCTOR_SPECIALITYResult> result = null;
+            // Take doctor in specific speciality in database
+            using (LinqDBDataContext data = new LinqDBDataContext())
+            {
+                result = await Task.Run(() =>
+                data.SP_LOAD_DOCTOR_IN_DOCTOR_SPECIALITY(SpecialityID).ToList());
+            }
+            // Assign value for each doctor
+            foreach (SP_LOAD_DOCTOR_IN_DOCTOR_SPECIALITYResult r in result)
+            {
+                doctor = new DoctorEntity();
+                doctor.DoctorID = r.Doctor_ID;
+                doctor.FirstName = r.First_Name;
+                doctor.LastName = r.Last_Name;
+                doctorList.Add(doctor);
+            }
+            return doctorList;
+        }
         #endregion
     }
 }
