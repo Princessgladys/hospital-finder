45944ee4222ac95e74e76b90fb5c847ba9c7f695
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-20 00:46:50 +0000

    Add method Appointment/Index()

--- trunk/Source Code/HospitalF/Controllers/AppointmentController.cs
+++ trunk/Source Code/HospitalF/Controllers/AppointmentController.cs
@@ -3,20 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 using HospitalF.App_Start;
 using HospitalF.Constant;
+using HospitalF.Models;
+using HospitalF.App_Start;
+using HospitalF.Utilities;
 
 namespace HospitalF.Controllers
 {
     public class AppointmentController : Controller
     {
+        public static List<Speciality> specialityList = null;
+        public static int hospitalID = 64;
         //
         // GET: /Appointment/
         [LayoutInjecter(Constants.HomeLayout)]
-        public ActionResult CreateAppointment()
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                //load list of speciality
+                specialityList = await SpecialityUtil.LoadSpecialityByHospitalIDAsync(hospitalID);
+                ViewBag.SpecialityList = new SelectList(specialityList, Constants.SpecialityID, Constants.SpecialityName);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(Constants.HomeErrorPage, Constants.ErrorController);
+            }
+            return View();
+        }
+        /// <summary>
+        /// GET: /Appointment/Index
+        /// </summary>
+        /// <param name="model">AppointmentModels</param>
+        /// <returns>Task[ActionResult]</returns>
+        [HttpGet]
+        [LayoutInjecter(Constants.HomeLayout)]
+        public async Task<ActionResult> CreateAppointment(AppointmentModels model)
         {
             return View();
         }
-
     }
 }
