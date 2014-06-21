03248fb625d009d49002836a4d7fa2c9fc538fe6
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-06-21 16:58:11 +0000

    modify script Hospital_Speciality.sql

--- trunk/Source Code/Store Procedure and Functions/Meta data/Hospital_Speciality.sql
+++ trunk/Source Code/Store Procedure and Functions/Meta data/Hospital_Speciality.sql
@@ -28,7 +28,7 @@ INSERT INTO Hospital_Speciality VALUES
 	(26, 36, 'False', 'True'),
 	(26, 40, 'False', 'True'),
 	(26, 43, 'False', 'True'),
-	(41, 40, 'False', 'True')
+	(41, 40, 'False', 'True'),
 	
 	-- HCM Family Medical Practice
 	
@@ -38,3 +38,11 @@ INSERT INTO Hospital_Speciality VALUES
 
 	-- Bệnh viện đa khoa quốc tế Vũ Anh
 	
+	 -- Hospital_ID=25: Bệnh viện Hoàn Mỹ
+     -- Hospital_ID=68: Bệnh viện Tai Mũi Họng
+     -- Hospital_ID=66: Phòng khám chuyên khoa mắt - American Eye Center
+(25,7,0,1),		(25,42,0,1),	(25,33,0,1),	(25,24,0,1),	(25,11,0,1),	(68,31,0,1),
+(25,6,0,1),		(25,41,0,1),	(25,31,0,1),	(25,20,0,1),	(25,1,0,1),		(68,22,0,1),
+(25,48,0,1),	(25,37,0,1),	(25,28,0,1),	(25,19,0,1),	(68,48,0,1),	(68,11,0,1),
+(25,46,0,1),	(25,34,0,1),	(25,26,0,1),	(25,13,0,1),	(68,37,1,1),	(68,1,0,1),
+(66,29,1,1)	
