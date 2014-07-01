0142734d484dfa0445ac111a5f27cb70172e5520
khoavnase60817@fpt.edu.vn <khoavnase60817@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-01 05:44:33 +0000

    Login using custom authentication with membership provider and role provider

---
+++ trunk/Source Code/HospitalF/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using HospitalF.Models;
+
+namespace HospitalF.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl = "")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return LogOut();
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl = "")
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Email, model.Password))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(model.Email, model.RememberMe);
+                }
+
+                ModelState.AddModelError("", "Incorrect username and/or password");
+            }
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account", null);
+        }
+
+    }
+}
