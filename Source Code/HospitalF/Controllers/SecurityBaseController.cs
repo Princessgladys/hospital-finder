5ea31d2bec8fb8683569dd0b455635bb0119de7e
vietlpse60706@fpt.edu.vn <vietlpse60706@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-02 08:21:23 +0000

    [vietlp] Update source code.

---
+++ Source Code/HospitalF/Controllers/SecurityBaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HospitalF.Models;
+
+namespace HospitalF.Controllers
+{
+    public class SecurityBaseController : Controller
+    {
+        //
+        // GET: /SecurityBase/
+
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (!string.IsNullOrEmpty(SimpleSessionPersister.Username))
+            {
+                filterContext.HttpContext.User =
+                    new CustomPrincipal(
+                        new CustomIdentity(
+                            SimpleSessionPersister.Username, SimpleSessionPersister.Role));
+            }
+            base.OnAuthorization(filterContext);
+        }
+
+    }
+}
