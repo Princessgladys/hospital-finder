8fed8c6f9ad79477fea4e1dd2794d3db9c17b557
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-02 05:29:33 +0000

    Remove ending white space in HomeModels. Add CompareString() method in StringUtil that implement Algorithm of calculating the percentage of similarity between 2 strings

--- trunk/Source Code/HospitalF/Utilities/StringUtil.cs
+++ trunk/Source Code/HospitalF/Utilities/StringUtil.cs
@@ -130,6 +130,95 @@ namespace HospitalF.Utilities
 
         #endregion
 
+        #region Algorithm to calculate the percentage of similarity between 2 strings
+
+        /// <summary>
+        /// Take pairs of letters in a word
+        /// </summary>
+        /// <param name="inputWord">Input word</param>
+        /// <returns>List[string] that contains pairs of letters in an input word</returns>
+        private static List<string> TakePairsOfLetters(string inputWord)
+        {
+            // Declare a list to store pairs of letters
+            List<string> pairsList = new List<string>();
+            // Count number of pairs in string
+            int numberOfPairs = inputWord.Length - 1;
+
+            // Add pairs of letters in input string to list
+            for (int n = 0; n < numberOfPairs; n++)
+            {
+                pairsList.Add(inputWord.Substring(n, 2));
+            }
+
+            // Return list of pairs
+            return pairsList;
+        }
+
+        /// <summary>
+        /// Take pairs of letters of each word in  list of tokens
+        /// </summary>
+        /// <param name="tokenList">List of tokens</param>
+        /// <returns>
+        /// List[string] that contains all pairs in each word
+        /// in a list of tokens
+        /// </returns>
+        private static List<String> TakePairsOfLettersInWord(List<string> tokenList)
+        {
+            // Declare a list to store all pairs of letters in token list
+            List<string> allPairs = new List<string>();
+            // Declare a list to store pairs of letters in a word
+            List<string> pairsInWord = new List<string>();
+
+            // Take pairs of letters in each word of token list
+            foreach (string word in tokenList)
+            {
+                pairsInWord = TakePairsOfLetters(word);
+                foreach(string pair in pairsInWord)
+                {
+                    allPairs.Add(pair);
+                }
+            }
+
+            // Return all pairs of letters in a list of tokens
+            return allPairs;
+        }
+
+        /// <summary>
+        /// Calculate the percentage of similarity between 2 strings
+        /// </summary>
+        /// <param name="inputStr1">Input string 1</param>
+        /// <param name="inputStr2">Input string 2</param>
+        /// <returns>Percentage of similarity between string 1 and string 2</returns>
+        public static float CompareString(string inputStr1, string inputStr2)
+        {
+            // Declare variable to store union and intersection of pairs in 
+            // string 1 and string 2
+            int union = 0;
+            int intersection = 0;
+
+            // Tokenizer two string and take pairs of letters in them
+            List<string> pairsInStr1 = TakePairsOfLettersInWord(StringTokenizer(inputStr1.Trim()));
+            List<string> pairsInStr2 = TakePairsOfLettersInWord(StringTokenizer(inputStr2.Trim()));
+            union = pairsInStr1.Count + pairsInStr2.Count;
+
+            // Calculate intersection between 2 input strings
+            foreach (string pairStr1 in pairsInStr1)
+            {
+                foreach (string pairStr2 in pairsInStr2)
+                {
+                    if (pairStr1.Equals(pairStr2))
+                    {
+                        intersection++;
+                    }
+                }
+            }
+
+            // Return percentage of similarity between 2 input strings
+            return ((2 * intersection) * 1.0f) / (union * 1.0f);
+        }
+
+        #endregion
+
         /// <summary>
         /// Split words in a string to every token
         /// </summary>
@@ -165,7 +254,7 @@ namespace HospitalF.Utilities
             string result = string.Empty;
             for (int n = beginIndex; n <= endIndex; n++)
             {
-                result += " " + tokenList[n];
+                result += Constants.WhiteSpace + tokenList[n];
             }
             // Remove leading white space and return value
             return result.Trim();
