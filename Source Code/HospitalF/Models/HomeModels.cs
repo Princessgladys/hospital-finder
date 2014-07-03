7eeeac46ce27db35c29ef2d5750a228a97540365
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-03 09:24:02 +0000

    Add validation for Homepage and Remove model validation from HomeModel

--- Source Code/HospitalF/Models/HomeModels.cs
+++ Source Code/HospitalF/Models/HomeModels.cs
@@ -20,7 +20,6 @@ namespace HospitalF.Models
         /// <summary>
         /// Get/Set value for property SearchValue
         /// </summary>
-        [Display(Name = Constants.SearchValue)]
         public string SearchValue { get; set; }
 
         /// <summary>
@@ -31,8 +30,6 @@ namespace HospitalF.Models
         /// <summary>
         /// Get/Set value for property CityID
         /// </summary>
-        [Display(Name = Constants.City)]
-        [Required(ErrorMessage = ErrorMessage.CEM011)]
         public int CityID { get; set; }
 
         /// <summary>
@@ -43,8 +40,6 @@ namespace HospitalF.Models
         /// <summary>
         /// Get/Set value for property DistrictID
         /// </summary>
-        [Display(Name = Constants.District)]
-        [Required(ErrorMessage = ErrorMessage.CEM011)]
         public int DistrictID { get; set; }
 
         /// <summary>
@@ -65,7 +60,6 @@ namespace HospitalF.Models
         /// <summary>
         /// Get/Set value for property SpecialityID
         /// </summary>
-        [Display(Name = Constants.Speciality)]
         public int SpecialityID { get; set; }
 
         /// <summary>
@@ -76,46 +70,42 @@ namespace HospitalF.Models
         /// <summary>
         /// Get/Set value for property DiseaseID
         /// </summary>
-        [Display(Name = Constants.Disease)]
         public int DiseaseID { get; set; }
 
         /// <summary>
         /// Get/Set value for property DiseaseName
         /// </summary>
-        [Display(Name = Constants.Disease)]
-        [StringLength(64, ErrorMessage = ErrorMessage.CEM003)]
         public string DiseaseName { get; set; }
 
         /// <summary>
         /// Get/Set value for property CurrentLocation
         /// </summary>
-        [Display(Name = Constants.CurrentLocation)]
         public string CurrentLocation { get; set; }
 
         /// <summary>
         /// Get/Set value for property AppointedLocation
         /// </summary>
-        [Display(Name = Constants.AppointedAddress)]
-        [StringLength(128, ErrorMessage = ErrorMessage.CEM003)]
         public string AppointedAddress { get; set; }
 
         /// <summary>
         /// Get/Set value for property HospitalName
         /// </summary>
-        [Display(Name = Constants.HospitalAddress)]
         public string HospitalName { get; set; }
 
         /// <summary>
         /// Get/Set value for property Coordinate
         /// </summary>
-        [Display(Name = Constants.Coordinate)]
         public string Coordinate { get; set; }
 
         /// <summary>
         /// Get/Set value for property HospitalAddres
         /// </summary>
-        [Display(Name = Constants.HospitalAddress)]
-        public string HospitalAddres { get; set; }
+        public string HospitalAddress { get; set; }
+
+        /// <summary>
+        /// Get/Set value for property Radius
+        /// </summary>
+        public float Radius { get; set; }
 
         #endregion
 
