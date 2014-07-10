ee7212ecb67a5886f35d0769d5f444c16d769631
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-10 09:48:39 +0000

    Update Stored Procedure SP_NORMAL_SEARCH_HOSPITAL (Not yet finished)

--- Source Code/Store Procedure and Functions/Stored Procedure/SP_NORMAL_SEARCH_HOSPITAL.sql
+++ Source Code/Store Procedure and Functions/Stored Procedure/SP_NORMAL_SEARCH_HOSPITAL.sql
@@ -5,60 +5,219 @@ IF OBJECT_ID('[SP_NORMAL_SEARCH_HOSPITAL]', 'P') IS NOT NULL
 	DROP PROCEDURE SP_NORMAL_SEARCH_HOSPITAL
 GO
 CREATE PROCEDURE SP_NORMAL_SEARCH_HOSPITAL
-	@HospitalName NVARCHAR(128),
-	@CityID INT,
-	@DistrictID INT,
-	@SpecialityID INT,
-	@DiseaseName NVARCHAR(64)
+	@WhatPhrase NVARCHAR(128),
+	@CityName NVARCHAR(32),
+	@DistrictName NVARCHAR(32),
+	@PercentageOfSimilarity FLOAT
 AS
 BEGIN
-	-- CHECK IF HOSPITAL ID IS NOT NULL
-	IF (@HospitalName IS NOT NULL)
+	-- VARIABLE FOR ROW NUMBER
+	DECLARE @RowNumber INT
+
+	-- VARIABLES FOR HOSPITALS
+	DECLARE @TotalHospital INT
+	DECLARE @HospitalName NVARCHAR(64)
+	
+	-- TRANSFORM @WhatPhrase to non-diacritic Vietnamese
+	SET @WhatPhrase = [dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE] (@WhatPhrase)
+	
+	-- CHECK IF THERE IS ANY HOSPITAL NAME IN @WhatPhrase
+	SET @TotalHospital = (SELECT COUNT(h.Hospital_ID)
+						  FROM Hospital h)
+	SET @RowNumber = 1
+
+	WHILE (@RowNumber <= @TotalHospital)
 	BEGIN
-		SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
-			   h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Start_Time,
-			   h.End_Time, h.Coordinate, h.Short_Description, h.Full_Description,
-			   h.Is_Allow_Appointment, h.Is_Active
-		FROM Hospital h
-		WHERE h.Hospital_Name LIKE (N'%' + @HospitalName + N'%')
-		ORDER BY h.Hospital_ID
-		RETURN;
+		-- GET HOSPITAL NAME FROM DATABASE
+		SELECT @HospitalName = LOWER([dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE] (h.Hospital_Name))
+							   FROM (SELECT ROW_NUMBER()
+									 OVER (ORDER BY TokenList.ID ASC) AS RowNumber, h.Hospital_Name
+									 FROM Hospital h) AS h
+							   WHERE RowNumber = @RowNumber
+
+		-- FIND MATCHED RESULT
+		IF ([dbo].[FU_IS_PATTERN_MATCHED] (@HospitalName, @WhatPhrase) = 1)
+		BEGIN
+			-- QUERY HOSPITAL NAME IN DATABASE
+			SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
+				   h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Start_Time,
+				   h.End_Time, h.Coordinate, h.Short_Description, h.Full_Description,
+				   h.Is_Allow_Appointment, h.Is_Active
+			FROM Hospital h
+			WHERE LOWER([dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE] (h.Hospital_Name)) LIKE
+				  (N'%' + @HospitalName + N'%') OR
+				  [dbo].[FU_STRING_COMPARE]
+					(LOWER([dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE]
+						(h.Hospital_Name)), @HospitalName) >= @PercentageOfSimilarity
+			RETURN;
+		END
+
+		SET @RowNumber += 1
 	END
 
-	IF (@CityID = 0)
-		SET @CityID = NULL
+	-- VARIABLES FOR SPECIALITY
+	DECLARE @TotalSpeciality INT
+	DECLARE @SpecialityName NVARCHAR(64)
+	DECLARE @IsHaveSpeciality INT = 0
 
-	iF (@DistrictID = 0)
-		SET @DistrictID = NULL
+	-- CHECK IF THERE IS ANY SPECIALITY NAME IN @WhatPhrase
+	SET @TotalSpeciality = (SELECT COUNT(s.Speciality_ID)
+							FROM Speciality s)
+	SET @RowNumber = 1
 
-	IF (@SpecialityID = 0)
-		SET @SpecialityID = NULL
-
-	IF (@DiseaseName IS NOT NULL)
+	WHILE (@RowNumber < @TotalSpeciality)
 	BEGIN
-		IF ([dbo].[FU_REMOVE_WHITE_SPACE](@DiseaseName) = '')
-			SET @DiseaseName = NULL
+		-- GET SPECIALITY NAME FROM DATABASE
+		SELECT @SpecialityName = LOWER([dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE] (s.Speciality_Name))
+								 FROM (SELECT ROW_NUMBER()
+									   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, s.Speciality_Name
+									   FROM Speciality s) AS s
+								 WHERE RowNumber = @RowNumber
+
+		-- FIND MATCHED RESULT
+		IF ([dbo].[FU_IS_PATTERN_MATCHED] (@WhatPhrase, @SpecialityName) = 1)
+		BEGIN
+			SET @IsHaveSpeciality = 1
+			BREAK
+		END
+
+		SET @RowNumber += 1
 	END
 
-	-- SET VALUE FOR 'WHAT' and 'WHERE' PHRASE
-	DECLARE @WhatPhrase INT,
-			@WherePhrase INT
+	-- VARIABLE FOR DISEASE
+	DECLARE @TotalDisease INT
+	DECLARE @DiseaseName NVARCHAR(64)
+	DECLARE @IsHaveDisease INT = 0
 
-	IF (@SpecialityID IS NULL AND @DiseaseName IS NULL)
-		SET @WhatPhrase = 0
-	ELSE
-		SET @WhatPhrase = 1
+	-- CHECK IF THERE IS ANY DISEASE NAME IN @WhatPhrase
+	SET @TotalDisease = (SELECT COUNT(d.Disease_ID)
+						 FROM Disease d)
+	SET @RowNumber = 1
 
-	IF (@CityID IS NULL AND @DistrictID IS NULL)
+	WHILE (@RowNumber < @TotalDisease)
+	BEGIN
+		-- GET DISEASE NAME FROM DATABASE
+		SELECT @DiseaseName = LOWER([dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE] (d.Disease_Name))
+							  FROM (SELECT ROW_NUMBER()
+									OVER (ORDER BY TokenList.ID ASC) AS RowNumber, d.Disease_Name
+									FROM Disease d) AS d
+							  WHERE RowNumber = @RowNumber
+
+		-- FIND MATCHED RESULT
+		IF ([dbo].[FU_IS_PATTERN_MATCHED] (@WhatPhrase, @DiseaseName) = 1)
+		BEGIN
+			SET @IsHaveDisease = 1
+			BREAK
+		END
+
+		SET @RowNumber += 1
+	END
+
+	-- VARIABLE FOR WHERE PHRASE
+	DECLARE @WherePhrase INT
+
+	IF (@CityName IS NULL AND @DistrictName IS NULL)
 		SET @WherePhrase = 0
 	ELSE
 		SET @WherePhrase = 1
 
-	-- CHECK IF ALL PARAMETERS ARE NULL
-	IF ((@WhatPhrase = 0) AND (@WherePhrase = 0))
+	-- VARIABLES FOR SEARCH QUERY
+	DECLARE @SelectPhrase NVARCHAR(512) = NULL
+	SET @SelectPhrase = N'SELECT DISTINCT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,' +
+						N'h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Start_Time,' +
+						N'h.End_Time, h.Coordinate, h.Short_Description, h.Full_Description,' +
+						N'h.Is_Allow_Appointment, h.Is_Active'
+
+	DECLARE @FromPhrase NVARCHAR(512) = NULL
+	SET @FromPhrase = N'FROM Hospital h, Hospital_Speciality hs, Speciality_Disease sd'
+
+	DECLARE @ConditionPhrase NVARCHAR(512) = NULL
+	SET @ConditionPhrase = N'WHERE h.Is_Active = ''True'''
+
+	DECLARE @OrderPhrase NVARCHAR(512)
+	SET @OrderPhrase = N'ORDER BY h.Hospital_Name'
+
+	DECLARE @SqlQuery NVARCHAR(512) = NULL
+	
+	-- CHECK IF ONLY WHAT PHRASE IS AVAILABLE
+	IF (@WherePhrase = 0)
 	BEGIN
-		RETURN;
+		SET @FromPhrase += N', Speciality s, Disease d'
+
+		SET @ConditionPhrase += CASE WHEN @IsHaveSpeciality = 1
+								THEN N''
+								ELSE '' END;
+
 	END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	-- CHECK IF BOTH CITY AND DISTRICT ARE NULL
 	IF ((@WherePhrase = 0) AND (@WhatPhrase = 1))
