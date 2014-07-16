f5651bbb593adf4d784fe8dc8b1f775f77de5ccf
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-16 04:59:49 +0000

    Remove error and add logic condition to check for @WherePhrase in Word_Dictionary table

--- trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_NORMAL_SEARCH_HOSPITAL.sql
+++ trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_NORMAL_SEARCH_HOSPITAL.sql
@@ -10,6 +10,13 @@ CREATE PROCEDURE SP_NORMAL_SEARCH_HOSPITAL
 	@DistrictName NVARCHAR(32)
 AS
 BEGIN
+	-- DEFINE @WherePhrase
+	DECLARE @WherePhrase INT
+	IF (@CityID != 0 AND @DistrictName IS NOT NULL)
+		SET @WherePhrase = 1
+	ELSE
+		SET @WherePhrase = 0
+
 	-- DEFAULT VALUE TO CONSIDER ANALYZING @WhatPhrase
 	DECLARE @Boundary INT = 30
 
@@ -17,16 +24,65 @@ BEGIN
 	DECLARE @HospitalList TABLE([Priority] INT IDENTITY(1,1) PRIMARY KEY,
 							    Hospital_ID INT)
 
-	INSERT INTO @HospitalList
-	SELECT (SELECT wh.Hospital_ID
-			FROM WordDictionary w, Word_Hospital wh
-			WHERE w.Word LIKE N'%' + @WhatPhrase + N'%' AND
-				  w.[Priority] != 1 AND
-				  w.Word_ID = wh.Word_ID)
-
 	-- DECLARE VARIABLE TO COUNT NUMBER OF RECORDS IN @HospitalList
-	DECLARE @TotalRecordInHospitalList INT = (SELECT COUNT([Priority])
-											  FROM @HospitalList)
+	DECLARE @TotalRecordInHospitalList INT =
+		(SELECT wh.Hospital_ID
+		 FROM WordDictionary w, Word_Hospital wh
+		 WHERE w.Word LIKE N'%' + @WhatPhrase + N'%' AND
+			   w.[Priority] != 1 AND
+			   w.Word_ID = wh.Word_ID)
+
+	IF (@TotalRecordInHospitalList > 0)
+	BEGIN
+		IF (@WherePhrase = 1)
+		BEGIN
+			INSERT INTO @HospitalList
+			SELECT (SELECT h.Hospital_ID
+					FROM Hospital h, District d,
+						 (SELECT wh.Hospital_ID
+						  FROM WordDictionary w, Word_Hospital wh
+						  WHERE w.Word LIKE N'%' + @WhatPhrase + N'%' AND
+								w.[Priority] != 1 AND
+								w.Word_ID = wh.Word_ID) w
+					WHERE h.City_ID = @CityID AND
+						  d.District_Name = @DistrictName AND
+						  d.District_ID = h.District_ID AND
+						  h.Hospital_ID = w.Hospital_ID)
+		END
+		ELSE
+		BEGIN
+			-- ADD THE HOSPITALS THAT HAVE CITY CONTAINS IN @WherePhrase
+			IF (@CityID != 0)
+			BEGIN
+				INSERT INTO @HospitalList
+				SELECT (SELECT h.Hospital_ID
+						FROM Hospital h, 
+							 (SELECT wh.Hospital_ID
+							  FROM WordDictionary w, Word_Hospital wh
+							  WHERE w.Word LIKE N'%' + @WhatPhrase + N'%' AND
+									w.[Priority] != 1 AND
+									w.Word_ID = wh.Word_ID) w
+						WHERE h.City_ID = @CityID AND
+							  w.Hospital_ID = h.Hospital_ID)
+			END
+
+			-- ADD THE HOSPITALS THAT HAVE DISTRICT CONTAINS IN @WherePhrase
+			IF (@DistrictName IS NOT NULL)
+			BEGIN
+				INSERT INTO @HospitalList
+				SELECT (SELECT h.Hospital_ID
+						FROM District d, Hospital h,
+							 (SELECT wh.Hospital_ID
+							  FROM WordDictionary w, Word_Hospital wh
+							  WHERE w.Word LIKE N'%' + @WhatPhrase + N'%' AND
+									w.[Priority] != 1 AND
+									w.Word_ID = wh.Word_ID) w
+						WHERE d.District_Name = @DistrictName AND
+							  d.District_ID = h.District_ID AND
+							  w.Hospital_ID = h.Hospital_ID)
+			END
+		END
+	END
 
 	IF (@TotalRecordInHospitalList > @Boundary)
 	BEGIN
@@ -95,7 +151,7 @@ BEGIN
 -------------------------------------------------------------------------------------------
 
 	-- CHECK IF @WherePhrase IS AVAILABLE
-	IF (@CityID != 0 AND @DistrictName IS NOT NULl)
+	IF (@WherePhrase = 1)
 	BEGIN
 		INSERT INTO @HospitalList
 		SELECT (SELECT h.Hospital_ID
@@ -264,7 +320,7 @@ BEGIN
 	-- CHECK IF @WherePhrase IS AVAILABLE
 	SET @DistrictName = [dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE] (@DistrictName)
 
-	IF (@CityID != 0 AND @DistrictName IS NOT NULl)
+	IF (@WherePhrase = 1)
 	BEGIN
 		INSERT INTO @HospitalList
 		SELECT (SELECT h.Hospital_ID
@@ -306,7 +362,7 @@ BEGIN
 	BEGIN
 		SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
 			   h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Ordinary_Start_Time,
-			   h.h.Holiday_End_Time, h.Coordinate, h.Short_Description, h.Full_Description,
+			   h.Holiday_End_Time, h.Coordinate, h.Short_Description, h.Full_Description,
 			   h.Is_Allow_Appointment, h.Is_Active, h.Holiday_Start_Time, h.Holiday_End_Time
 		FROM Hospital h, @HospitalList list
 		WHERE h.Hospital_ID = list.Hospital_ID
@@ -335,7 +391,7 @@ BEGIN
 	BEGIN
 		SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
 				h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Ordinary_Start_Time,
-				h.h.Holiday_End_Time, h.Coordinate, h.Short_Description, h.Full_Description,
+				h.Holiday_End_Time, h.Coordinate, h.Short_Description, h.Full_Description,
 				h.Is_Allow_Appointment, h.Is_Active, h.Holiday_Start_Time, h.Holiday_End_Time
 		FROM Hospital h, @HospitalList list
 		WHERE h.Hospital_ID = list.Hospital_ID
