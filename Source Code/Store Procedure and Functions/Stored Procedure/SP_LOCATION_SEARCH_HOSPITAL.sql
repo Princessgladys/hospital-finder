f3fb6ccb138129d92e6914ed3d7e68aaea088524
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-16 08:54:17 +0000

    Modify stored procedure to take value of Rating Score

--- Source Code/Store Procedure and Functions/Stored Procedure/SP_LOCATION_SEARCH_HOSPITAL.sql
+++ Source Code/Store Procedure and Functions/Stored Procedure/SP_LOCATION_SEARCH_HOSPITAL.sql
@@ -13,7 +13,8 @@ BEGIN
 	SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
 		   h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Ordinary_Start_Time,
 		   h.Ordinary_End_Time, h.Short_Description, h.Full_Description, h.Holiday_End_Time,
-		   h.Is_Allow_Appointment, h.Is_Active, h.Coordinate, h.Holiday_Start_Time,[dbo].[FU_CALCULATE_AVERAGE_RATING](h.Hospital_ID) AS Rating
+		   h.Is_Allow_Appointment, h.Is_Active, h.Coordinate, h.Holiday_Start_Time,
+		   [dbo].[FU_CALCULATE_AVERAGE_RATING] (h.Hospital_ID) AS Rating
 	FROM Hospital h
 	WHERE h.Is_Active = 'True' AND
 		  [dbo].[FU_GET_DISTANCE] 
@@ -21,6 +22,3 @@ BEGIN
 			 CONVERT(FLOAT, SUBSTRING(h.Coordinate, CHARINDEX(',', h.Coordinate) + 1, LEN(h.Coordinate))),
 			 @Latitude, @Longitude) <= CONVERT(INT, @Distance)
 END
-
-<-- EXEC -->
-EXEC SP_LOCATION_SEARCH_HOSPITAL 10.784075, 106.689859, 700
