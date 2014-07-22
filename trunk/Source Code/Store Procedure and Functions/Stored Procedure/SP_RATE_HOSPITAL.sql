c2ed6463b15b67977bd1a429369d7dc2f47b33a3
vietlpse60706@fpt.edu.vn <vietlpse60706@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-22 05:15:06 +0000

    [vietlp] Update SP.

---
+++ trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_RATE_HOSPITAL.sql
@@ -0,0 +1,35 @@
+-- SCRIPT TO SEARCH HOSPITALS
+-- USING LOCATION OPTION
+-- VIETLP
+IF OBJECT_ID('[SP_RATE_HOSPITAL]', 'P') IS NOT NULL
+	DROP PROCEDURE SP_RATE_HOSPITAL
+GO
+
+CREATE PROCEDURE SP_RATE_HOSPITAL
+	@User_ID INT,
+	@Hospital_ID INT,
+	@Score INT
+AS
+BEGIN
+	IF (1 <= @Score AND @Score <= 5)
+	BEGIN
+		BEGIN TRAN
+		BEGIN TRY
+			INSERT INTO Rating (Score, Hospital_ID, Created_Person)
+			VALUES (@Score, @Hospital_ID, @User_ID)
+			
+			DECLARE @Average_Score FLOAT
+			SET @Average_Score = [dbo].[FU_CALCULATE_AVERAGE_RATING] (@Hospital_ID)
+			
+			UPDATE Hospital
+			SET Score = @Average_Score
+			WHERE Hospital_ID = @Hospital_ID
+			
+			COMMIT TRAN
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRAN
+		END CATCH		
+	END
+END
+GO
