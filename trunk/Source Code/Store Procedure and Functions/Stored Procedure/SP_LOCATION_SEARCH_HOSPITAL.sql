6bb54f070a7df39a749369833c7a3397cef3a7a4
vietlpse60706@fpt.edu.vn <vietlpse60706@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-22 06:03:38 +0000

    [vietlp] Update SP.

--- trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_LOCATION_SEARCH_HOSPITAL.sql
+++ trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_LOCATION_SEARCH_HOSPITAL.sql
@@ -13,8 +13,7 @@ BEGIN
 	SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
 		   h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Ordinary_Start_Time,
 		   h.Ordinary_End_Time, h.Short_Description, h.Full_Description, h.Holiday_End_Time,
-		   h.Is_Allow_Appointment, h.Is_Active, h.Coordinate, h.Holiday_Start_Time,
-		   [dbo].[FU_CALCULATE_AVERAGE_RATING] (h.Hospital_ID) AS Rating
+		   h.Is_Allow_Appointment, h.Is_Active, h.Coordinate, h.Holiday_Start_Time, h.Rating
 	FROM Hospital h
 	WHERE h.Is_Active = 'True' AND
 		  [dbo].[FU_GET_DISTANCE] 
