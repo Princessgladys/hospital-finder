15a8378c82f48d464a753229461dd6f57f84d3fd
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-23 12:44:00 +0000

    Modify data for table Service and Facility. Add two new table ServiceType and FacilityType.

---
+++ Source Code/Store Procedure and Functions/Meta data/FacilityType.sql
@@ -0,0 +1,8 @@
+﻿-- SCRIPT DEFINE FACILITY TYPE FOR HOSPITAL
+
+INSERT INTO FacilityType VALUES
+	(N'Quản lý phương tiện'),
+	(N'Khu vực chuyên trách'),
+	(N'Tiện ích'),
+	(N'Phối cảnh'),
+	(N'Khu dịch vụ')
