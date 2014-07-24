a3c3945e95bcdc55eaca09214f002f0d86ae691d
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-24 12:27:01 +0000

    Update stored procedure SP_CHECK_NOT_DUPLICATED_HOSPITAL and SP_INSERT_HOSPITAL

--- trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_INSERT_HOSPITAL.sql
+++ trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_INSERT_HOSPITAL.sql
@@ -85,112 +85,121 @@ BEGIN
 		END
 
 		-- INSERT TO HOSPITAL_SPECIALITY TABLE
-		DECLARE @RowNumber INT = 1
-		DECLARE @TotalToken  INT = 0;
-		DECLARE @Token VARCHAR(4)
-		DECLARE @HospitalID INT = (SELECT TOP 1 Hospital_ID
-								   FROM Hospital
-								   ORDER BY Hospital_ID DESC)
-
-		SELECT @TotalToken = (SELECT COUNT(TokenList.ID)
-							  FROM [dbo].[FU_STRING_TOKENIZE] (@SpecialityList, '|') TokenList)
-
-		WHILE (@RowNumber <= @TotalToken)
+		IF (@SpecialityList != '')
 		BEGIN
-			SELECT @Token = (SELECT TokenList.Token
-							 FROM (SELECT ROW_NUMBER()
-								   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, TokenList.Token
-								   FROM [dbo].[FU_STRING_TOKENIZE] (@SpecialityList, '|') TokenList) AS TokenList
-							 WHERE RowNumber = @RowNumber)
-			INSERT INTO Hospital_Speciality
-			(
-				Hospital_ID,
-				Speciality_ID,
-				Is_Main_Speciality,
-				Is_Active
-			)
-			VALUES
-			(
-				@HospitalID,
-				@Token,
-				'False',
-				'True'
-			)
+			DECLARE @RowNumber INT = 1
+			DECLARE @TotalToken  INT = 0;
+			DECLARE @Token VARCHAR(4)
+			DECLARE @HospitalID INT = (SELECT TOP 1 Hospital_ID
+									   FROM Hospital
+									   ORDER BY Hospital_ID DESC)
 
-			IF @@ROWCOUNT = 0
+			SELECT @TotalToken = (SELECT COUNT(TokenList.ID)
+								  FROM [dbo].[FU_STRING_TOKENIZE] (@SpecialityList, '|') TokenList)
+
+			WHILE (@RowNumber <= @TotalToken)
 			BEGIN
-				ROLLBACK TRAN T;
-				RETURN 0;
+				SELECT @Token = (SELECT TokenList.Token
+								 FROM (SELECT ROW_NUMBER()
+									   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, TokenList.Token
+									   FROM [dbo].[FU_STRING_TOKENIZE] (@SpecialityList, '|') TokenList) AS TokenList
+								 WHERE RowNumber = @RowNumber)
+				INSERT INTO Hospital_Speciality
+				(
+					Hospital_ID,
+					Speciality_ID,
+					Is_Main_Speciality,
+					Is_Active
+				)
+				VALUES
+				(
+					@HospitalID,
+					@Token,
+					'False',
+					'True'
+				)
+
+				IF @@ROWCOUNT = 0
+				BEGIN
+					ROLLBACK TRAN T;
+					RETURN 0;
+				END
 			END
 		END
 
 		-- INSERT TO SERVCE TABLE
-		SET @RowNumber = 1
-		SET @TotalToken = 0;
-
-		SELECT @TotalToken = (SELECT COUNT(TokenList.ID)
-							  FROM [dbo].[FU_STRING_TOKENIZE] (@ServiceList, '|') TokenList)
-
-		WHILE (@RowNumber <= @TotalToken)
+		IF (@ServiceList != '')
 		BEGIN
-			SELECT @Token = (SELECT TokenList.Token
-							 FROM (SELECT ROW_NUMBER()
-								   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, TokenList.Token
-								   FROM [dbo].[FU_STRING_TOKENIZE] (@ServiceList, '|') TokenList) AS TokenList
-							 WHERE RowNumber = @RowNumber)
-			INSERT INTO Hospital_Service
-			(
-				Hospital_ID,
-				Service_ID,
-				Is_Active
-			)
-			VALUES
-			(
-				@HospitalID,
-				@Token,
-				'True'
-			)
+			SET @RowNumber = 1
+			SET @TotalToken = 0;
 
-			IF @@ROWCOUNT = 0
+			SELECT @TotalToken = (SELECT COUNT(TokenList.ID)
+								  FROM [dbo].[FU_STRING_TOKENIZE] (@ServiceList, '|') TokenList)
+
+			WHILE (@RowNumber <= @TotalToken)
 			BEGIN
-				ROLLBACK TRAN T;
-				RETURN 0;
+				SELECT @Token = (SELECT TokenList.Token
+								 FROM (SELECT ROW_NUMBER()
+									   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, TokenList.Token
+									   FROM [dbo].[FU_STRING_TOKENIZE] (@ServiceList, '|') TokenList) AS TokenList
+								 WHERE RowNumber = @RowNumber)
+				INSERT INTO Hospital_Service
+				(
+					Hospital_ID,
+					Service_ID,
+					Is_Active
+				)
+				VALUES
+				(
+					@HospitalID,
+					@Token,
+					'True'
+				)
+
+				IF @@ROWCOUNT = 0
+				BEGIN
+					ROLLBACK TRAN T;
+					RETURN 0;
+				END
 			END
 		END
 
 		-- INSERT TO FACILITY TABLE
-		SET @RowNumber = 1
-		SET @TotalToken = 0;
-
-		SELECT @TotalToken = (SELECT COUNT(TokenList.ID)
-							  FROM [dbo].[FU_STRING_TOKENIZE] (@FacilityList, '|') TokenList)
-
-		WHILE (@RowNumber <= @TotalToken)
+		IF (@FacilityList != '')
 		BEGIN
-			SELECT @Token = (SELECT TokenList.Token
-							 FROM (SELECT ROW_NUMBER()
-								   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, TokenList.Token
-								   FROM [dbo].[FU_STRING_TOKENIZE] (@FacilityList, '|') TokenList) AS TokenList
-							 WHERE RowNumber = @RowNumber)
-			INSERT INTO Hospital_Facility
-			(
-				Hospital_ID,
-				Facility_ID,
-				Is_Active
-			)
-			VALUES
-			(
-				@HospitalID,
-				@Token,
-				'True'
-			)
+			SET @RowNumber = 1
+			SET @TotalToken = 0;
 
-			IF @@ROWCOUNT = 0
+			SELECT @TotalToken = (SELECT COUNT(TokenList.ID)
+								  FROM [dbo].[FU_STRING_TOKENIZE] (@FacilityList, '|') TokenList)
+
+			WHILE (@RowNumber <= @TotalToken)
 			BEGIN
-				ROLLBACK TRAN T;
-				RETURN 0;
+				SELECT @Token = (SELECT TokenList.Token
+								 FROM (SELECT ROW_NUMBER()
+									   OVER (ORDER BY TokenList.ID ASC) AS RowNumber, TokenList.Token
+									   FROM [dbo].[FU_STRING_TOKENIZE] (@FacilityList, '|') TokenList) AS TokenList
+								 WHERE RowNumber = @RowNumber)
+				INSERT INTO Hospital_Facility
+				(
+					Hospital_ID,
+					Facility_ID,
+					Is_Active
+				)
+				VALUES
+				(
+					@HospitalID,
+					@Token,
+					'True'
+				)
+
+				IF @@ROWCOUNT = 0
+				BEGIN
+					ROLLBACK TRAN T;
+					RETURN 0;
+				END
 			END
-		END
+		END	
 	END
 	COMMIT TRAN T;
 
