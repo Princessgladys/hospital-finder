d1f3b70a7e84ae4fd8b52e2007d93e53689d4aa6
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-26 07:11:24 +0000

    add entity

---
+++ Source Code/HospitalF/Entities/FacilityEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalF.Entities
+{
+    public class FacilityEntity
+    {
+        #region Facility property
+        /// <summary>
+        /// Property for Service_ID attribute
+        /// </summary>
+        public int Facility_ID { get; set; }
+
+        /// <summary>
+        /// Property for Service_Name attribute
+        /// </summary>
+        public string Facility_Name { get; set; }
+        /// <summary>
+        /// Property for Type_ID attribute
+        /// </summary>
+        public int? Type_ID { get; set; }
+        /// <summary>
+        /// Property for Type_Name attribute
+        /// </summary>
+        public string Type_Name { get; set; }
+        #endregion
+    }
+}
---
+++ Source Code/HospitalF/Entities/ServiceEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalF.Entities
+{
+    public class ServiceEntity
+    {
+        #region Service property
+        /// <summary>
+        /// Property for Service_ID attribute
+        /// </summary>
+        public int Service_ID { get; set; }
+
+        /// <summary>
+        /// Property for Service_Name attribute
+        /// </summary>
+        public string Service_Name { get; set; }
+        /// <summary>
+        /// Property for Type_ID attribute
+        /// </summary>
+        public int? Type_ID { get; set; }
+        /// <summary>
+        /// Property for Type_Name attribute
+        /// </summary>
+        public string Type_Name { get; set; }
+        #endregion
+    }
+}
