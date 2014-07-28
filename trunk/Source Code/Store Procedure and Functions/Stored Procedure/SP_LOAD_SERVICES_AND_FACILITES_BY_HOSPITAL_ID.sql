0f9890eac8912d3cf998b0cf23029f1819eb895b
vietlpse60706@fpt.edu.vn <vietlpse60706@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-28 02:32:06 +0000

    [vietlp] Update SP.

--- trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_LOAD_SERVICES_AND_FACILITES_BY_HOSPITAL_ID.sql
+++ trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_LOAD_SERVICES_AND_FACILITES_BY_HOSPITAL_ID.sql
@@ -8,12 +8,32 @@ CREATE PROCEDURE SP_LOAD_SERVICES_BY_HOSPITAL_ID
 	@Hospital_ID INT
 AS
 BEGIN
-	SELECT s.Service_ID, Service_Name, Type_ID, Type_Name, Is_Active
+	SELECT s.Service_ID, s.Service_Name, st.Type_ID, st.Type_Name, hs.Is_Active
 	FROM Service s
 	LEFT JOIN ServiceType st ON s.Service_Type = st.Type_ID
 	LEFT JOIN Hospital_Service hs ON s.Service_ID = hs.Service_ID AND hs.Hospital_ID = @Hospital_ID
-	ORDER BY Type_ID
+	ORDER BY st.Type_ID
 END
 GO
 
 EXEC SP_LOAD_SERVICES_BY_HOSPITAL_ID 1
+GO
+----------------------------------------------------------------
+IF OBJECT_ID('[SP_LOAD_FACILITIES_BY_HOSPITAL_ID]', 'P') IS NOT NULL
+	DROP PROCEDURE SP_LOAD_FACILITIES_BY_HOSPITAL_ID
+GO
+
+CREATE PROCEDURE SP_LOAD_FACILITIES_BY_HOSPITAL_ID
+	@Hospital_ID INT
+AS
+BEGIN
+	SELECT f.Facility_ID, f.Facility_Name, ft.Type_ID, ft.Type_Name, hf.Is_Active
+	FROM Facility f
+	LEFT JOIN FacilityType ft ON f.Facility_Type = ft.Type_ID
+	LEFT JOIN Hospital_Facility hf ON f.Facility_ID = hf.Facility_ID AND hf.Hospital_ID = @Hospital_ID
+	ORDER BY ft.Type_ID
+END
+GO
+
+EXEC SP_LOAD_FACILITIES_BY_HOSPITAL_ID 1
+GO
