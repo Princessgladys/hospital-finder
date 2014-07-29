9fcf2ad86e0e511eb239a205a703c74e2cc237e9
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-29 06:55:07 +0000

    add DoctorController.cs modify AppointmentController

---
+++ trunk/Source Code/HospitalF/Controllers/DoctorController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HospitalF.App_Start;
+using HospitalF.Constant;
+using HospitalF.Models;
+using HospitalF.Utilities;
+
+namespace HospitalF.Controllers
+{
+    public class DoctorController : Controller
+    {
+
+        public static DoctorModels DoctorModel = null;
+
+        //
+        // GET: /Doctor/
+        [LayoutInjecter(Constants.HospitalUserLayout)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Get/UpdateDoctor
+        /// </summary>
+        /// <returns></returns>
+        [LayoutInjecter(Constants.HospitalUserLayout)]
+        public async Task<ActionResult> UpdateDoctor()
+        {
+            int doctorID=1;
+            int hospitalID = 68;
+            Doctor doctor = new Doctor();
+            DoctorModel=new DoctorModels();
+            using (LinqDBDataContext data = new LinqDBDataContext())
+            {
+                doctor = await Task.Run(()=>(
+                    from d in data.Doctors
+                    where d.Doctor_ID==doctorID
+                    select d).FirstOrDefault());
+            }
+            DoctorModel.DoctorID = doctor.Doctor_ID;
+            DoctorModel.Fullname = doctor.Last_Name + " " + doctor.First_Name;
+            DoctorModel.Experience = doctor.Experience;
+            DoctorModel.Degree = doctor.Degree;
+            DoctorModel.WorkingDay = doctor.Working_Day;
+            //specialiy of doctor
+            List<Speciality> SpecialityList = null;
+            SpecialityList=await SpecialityUtil.LoadSpecialityInDoctorSpeciality(doctorID);
+            List<String> specialityList=new List<string>();
+            foreach (Speciality sp in SpecialityList)
+            {
+                specialityList.Add(sp.Speciality_ID.ToString());
+            }
+            SpecialityList=await SpecialityUtil.LoadSpecialityByHospitalIDAsync(hospitalID);
+            ViewBag.SpecialityList = new SelectList(SpecialityList, Constants.SpecialityID, Constants.SpecialityName);
+            DoctorModel.SelectedSpecialities = specialityList;
+
+            return View(DoctorModel);
+        }
+
+        public ActionResult UpdateDoctor(DoctorModels model)
+        {
+            return RedirectToAction(Constants.IndexAction,Constants.HospitalController);
+        }
+
+    }
+}
