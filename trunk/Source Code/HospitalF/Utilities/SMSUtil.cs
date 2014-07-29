77169cb04abdaf966d78aff74f3815c4e183a321
anhdth60434@fpt.edu.vn <anhdth60434@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-07-29 06:56:35 +0000

    add SMSUtil.cs

---
+++ trunk/Source Code/HospitalF/Utilities/SMSUtil.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace HospitalF.Utilities
+{
+    public class SMSUtil
+    {
+        const string APIKey = "899211D11D10709C5CC8F60BF121A6";//Dang ky tai khoan tai esms.vn de lay key
+        const string SecretKey = "470C5EC4CBFF896523B24B637DCFEE";
+
+        public static void SendSMS(string phone, string message)
+        {
+            string url = "http://api.esms.vn/MainService.svc/xml/SendMultipleSMSBrandname/";
+            // declare ascii encoding
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            string strResult = string.Empty;
+            // sample xml sent to Service & this data is sent in POST
+
+
+
+
+            string customers = "";
+
+            string[] lstPhone = phone.Split(',');
+
+            for (int i = 0; i < lstPhone.Count(); i++)
+            {
+                customers = customers + @"<CUSTOMER>"
+                                + "<PHONE>" + lstPhone[i] + "</PHONE>"
+                                + "</CUSTOMER>";
+            }
+
+
+            string SampleXml = @"<RQST>"
+                               + "<APIKEY>" + APIKey + "</APIKEY>"
+                               + "<SECRETKEY>" + SecretKey + "</SECRETKEY>"
+                               + "<SMSTYPE>2</SMSTYPE>"
+                               + "<BRANDNAME>KIM.HOANG</BRANDNAME>"
+                               + "<CONTENT>" + message + "</CONTENT>"
+                               + "<CONTACTS>" + customers + "</CONTACTS>"
+
+
+           + "</RQST>";
+            string postData = SampleXml.Trim().ToString();
+            // convert xmlstring to byte using ascii encoding
+            byte[] data = encoding.GetBytes(postData);
+            // declare httpwebrequet wrt url defined above
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            // set method as post
+            webrequest.Method = "POST";
+            webrequest.Timeout = 500000;
+            // set content type
+            webrequest.ContentType = "application/x-www-form-urlencoded";
+            // set content length
+            webrequest.ContentLength = data.Length;
+            // get stream data out of webrequest object
+            Stream newStream = webrequest.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+            // declare & read response from service
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+
+            // set utf8 encoding
+            Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+            // read response stream from response object
+            StreamReader loResponseStream =
+                new StreamReader(webresponse.GetResponseStream(), enc);
+            // read string from stream data
+            strResult = loResponseStream.ReadToEnd();
+            // close the stream object
+            loResponseStream.Close();
+            // close the response object
+            webresponse.Close();
+            // below steps remove unwanted data from response string
+            strResult = strResult.Replace("</string>", "");
+        }
+        public string GetConfirmCode()
+        {
+            return "";
+        }
+    }
+}
