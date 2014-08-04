0826ba8f8c939f4345cb2a8442fec83ae57aac0d
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-08-04 18:54:12 +0000

    Add function FU_TAKE_NUMBER_OF_TAG and update comment for function FU_TAKE_RATING_COUNT

---
+++ trunk/Source Code/Store Procedure and Functions/Functions/FU_TAKE_NUMBER_OF_TAG.sql
@@ -0,0 +1,30 @@
+-- FUNCTION TO RETURN NUMBER OF TAGS OF A SPECIFIC HOSPITAL
+-- SONNX
+IF OBJECT_ID('FU_TAKE_NUMBER_OF_TAG', 'FN') IS NOT NULL
+	DROP FUNCTION [FU_TAKE_NUMBER_OF_TAG]
+GO
+CREATE FUNCTION [dbo].[FU_TAKE_NUMBER_OF_TAG]
+(
+	@HospitalID INT,
+	@WhatPhrase NVARCHAR(4000)
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @NumberOfTag INT
+
+	SELECT @NumberOfTag = (SELECT COUNT(*)
+						   FROM (SELECT w.Word_ID
+							     FROM WordDictionary w, Word_Hospital wh
+							     WHERE w.[Type] = 2 AND
+									   FREETEXT (w.Word, @WhatPhrase) AND
+									   w.Word_ID = wh.Word_ID AND
+									   wh.Hospital_ID = @HospitalID) w)
+
+	IF (@NumberOfTag IS NULL)
+		RETURN 0;
+	ELSE
+		RETURN @NumberOfTag;
+
+	RETURN 0;
+END
