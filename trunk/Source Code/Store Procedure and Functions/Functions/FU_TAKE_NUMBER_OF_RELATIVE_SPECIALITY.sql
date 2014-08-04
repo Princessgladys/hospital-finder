d12d6369d6e7eb37b837a49d6b77394bc3f56b58
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-08-04 20:50:57 +0000

    Add function FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY and FU_TAKE_NUMBER_OF_RELATIVE_DISEASE. Change name of FU_TAKE_NUMBER_OF_TAG TO FU_TAKE_NUMBER_OF_RELATIVE_TAG

---
+++ trunk/Source Code/Store Procedure and Functions/Functions/FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY.sql
@@ -0,0 +1,30 @@
+-- FUNCTION TO RETURN NUMBER OF RELATIVE SPECIALITY OF A SPECIFIC HOSPITAL
+-- SONNX
+IF OBJECT_ID('FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY', 'FN') IS NOT NULL
+	DROP FUNCTION [FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY]
+GO
+CREATE FUNCTION [dbo].[FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY]
+(
+	@HospitalID INT,
+	@WhatPhrase NVARCHAR(4000)
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @NumberOfSpeciality INT
+
+	SELECT @NumberOfSpeciality = (SELECT COUNT(*)
+								  FROM (SELECT s.Speciality_ID
+										FROM Speciality s, Hospital h, Hospital_Speciality hs
+										WHERE FREETEXT (Speciality_Name, @WhatPhrase) AND
+											  s.Speciality_ID = hs.Speciality_ID AND
+											  h.Hospital_ID = hs.Hospital_ID AND
+											  h.Hospital_ID = @HospitalID) s)
+
+	IF (@NumberOfSpeciality IS NULL)
+		RETURN 0;
+	ELSE
+		RETURN @NumberOfSpeciality;
+
+	RETURN 0;
+END
