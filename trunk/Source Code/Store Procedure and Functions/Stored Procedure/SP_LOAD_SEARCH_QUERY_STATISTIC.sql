4ddcc14497c59dc5e3ab05e09d995301e70ae57b
vietlpse60706@fpt.edu.vn <vietlpse60706@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-08-11 11:52:36 +0000

    [vietlp] Update SP.

---
+++ trunk/Source Code/Store Procedure and Functions/Stored Procedure/SP_LOAD_SEARCH_QUERY_STATISTIC.sql
@@ -0,0 +1,19 @@
+-- SCRIPT TO INSERT SEARCH QUERY
+-- VIETLP
+IF OBJECT_ID('[SP_LOAD_SEARCH_QUERY_STATISTIC]', 'P') IS NOT NULL
+	DROP PROCEDURE SP_LOAD_SEARCH_QUERY_STATISTIC
+GO
+
+CREATE PROCEDURE SP_LOAD_SEARCH_QUERY_STATISTIC
+(
+	@From_Date DATETIME,
+	@To_Date DATETIME
+)
+AS
+BEGIN
+	SELECT sd.Sentence, sd.Search_Date, sd.Result_Count
+	FROM SentenceDictionary sd
+	WHERE sd.Search_Date BETWEEN @From_Date AND @To_Date
+	GROUP BY sd.Sentence, sd.Search_Date, sd.Result_Count
+	ORDER BY sd.Search_Date DESC
+END
