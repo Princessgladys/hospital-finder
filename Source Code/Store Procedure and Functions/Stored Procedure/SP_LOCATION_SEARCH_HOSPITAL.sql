2e86a89c6dc852e0f23d7af250b77efda49132ab
vietlpse60706@fpt.edu.vn <vietlpse60706@fpt.edu.vn@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-08-19 20:23:38 +0000

    [vietlp] + Add SP_LOAD_SUGGEST_SEARCH_QUERY.sql + Update SP_INSERT_SEARCH_QUERY.sql + Update SP_LOAD_SEARCH_QUERY_STATISTIC.sql + Update SP_LOCATION_SEARCH_HOSPITAL.sql

--- Source Code/Store Procedure and Functions/Stored Procedure/SP_LOCATION_SEARCH_HOSPITAL.sql
+++ Source Code/Store Procedure and Functions/Stored Procedure/SP_LOCATION_SEARCH_HOSPITAL.sql
@@ -12,9 +12,13 @@ AS
 BEGIN
 	SELECT h.Hospital_ID, h.Hospital_Name, h.[Address], h.Ward_ID, h.District_ID,
 		   h.City_ID, h.Phone_Number, h.Fax, h.Email, h.Website, h.Ordinary_Start_Time,
-		   h.Ordinary_End_Time, h.Short_Description, h.Full_Description, h.Holiday_End_Time,
+		   h.Ordinary_End_Time, h.Full_Description, h.Holiday_End_Time,
 		   h.Is_Allow_Appointment, h.Is_Active, h.Coordinate, h.Holiday_Start_Time, h.Rating,
-		   c.City_Name, d.District_Name, w.Ward_Name
+		   c.City_Name, d.District_Name, w.Ward_Name,
+		   [dbo].[FU_GET_DISTANCE] 
+			(CONVERT(FLOAT, SUBSTRING(h.Coordinate, 1, CHARINDEX(',', h.Coordinate) - 1)),
+			 CONVERT(FLOAT, SUBSTRING(h.Coordinate, CHARINDEX(',', h.Coordinate) + 1, LEN(h.Coordinate))),
+			 @Latitude, @Longitude) AS Distance
 		   
 	FROM City c, District d, Ward w, Hospital h
 	WHERE h.Is_Active = 'True' AND
@@ -23,7 +27,13 @@ BEGIN
 			(CONVERT(FLOAT, SUBSTRING(h.Coordinate, 1, CHARINDEX(',', h.Coordinate) - 1)),
 			 CONVERT(FLOAT, SUBSTRING(h.Coordinate, CHARINDEX(',', h.Coordinate) + 1, LEN(h.Coordinate))),
 			 @Latitude, @Longitude) <= CONVERT(INT, @Distance)
+	ORDER BY Distance
 END
 GO
+
 EXEC SP_LOCATION_SEARCH_HOSPITAL 10.8525, 106.6225, 10000
 GO
+
+SELECT Hospital_Name, Address, Coordinate
+FROM Hospital
+ORDER BY Coordinate
