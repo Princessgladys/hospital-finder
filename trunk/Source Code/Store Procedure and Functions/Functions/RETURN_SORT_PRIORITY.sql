414d7736a0f68d5fbe8ad4c8661db906083824af
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-08-20 17:35:41 +0000

    Improve running performance of stored procedure SP_NORMAL_SEARCH_HOSPITAL

---
+++ trunk/Source Code/Store Procedure and Functions/Functions/RETURN_SORT_PRIORITY.sql
@@ -0,0 +1,67 @@
+-- FUNCTION TO RETURN SORT PRIORITY IN NORMAL SEARCH MODE
+-- SONNX
+IF OBJECT_ID('RETURN_SORT_PRIORITY', 'FN') IS NOT NULL
+	DROP FUNCTION [RETURN_SORT_PRIORITY]
+GO
+CREATE FUNCTION RETURN_SORT_PRIORITY
+(
+	@HospitalID INT,
+	@Index INT,
+	@WhatPhrase NVARCHAR(128)
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @Priority INT = 0
+
+	-- DEFINE PRIORITY OF SORT ORDER
+	DECLARE @ExactlyPriorityOfTag INT = 10000
+	DECLARE @ExactlyPriorityOfSpeciality INT = 10000
+	DECLARE @ExactlyPriorityOfDisease INT = 10000
+
+	DECLARE @RelativePriorityOfTag INT = 100
+	DECLARE @RelativePriorityOfSpeciality INT = 100
+	DECLARE @RelativePriorityOfDisease INT = 100
+	
+	DECLARE @NonDiacriticPriorityOfTag INT = 10
+	DECLARE @NonDiacriticPriorityOfSpeciality INT = 10
+	DECLARE @NonDiacriticPriorityOfDisease INT = 10
+
+	-- DEFINE INDEX NUMBER FOR SORT ORDER
+	DECLARE @ExactlyIndexOfTag INT = 1
+	DECLARE @ExactlyIndexOfSpeciality INT = 2
+	DECLARE @ExactlyIndexOfDisease INT = 3
+
+	DECLARE @RelativeIndexOfTag INT = 8
+	DECLARE @RelativeIndexOfSpeciality INT = 9
+	DECLARE @RelativeIndexOfDisease INT = 10
+	
+	DECLARE @NonDiacriticIndexOfTag INT = 11
+	DECLARE @NonDiacriticIndexOfSpeciality INT = 12
+	DECLARE @NonDiacriticIndexOfDisease INT = 13
+
+	-- RETURN PRIORITY OF EXACTLY TAG
+	SELECT @Priority = 
+		CASE @Index
+			WHEN @ExactlyIndexOfTag THEN @ExactlyPriorityOfTag
+			WHEN @ExactlyIndexOfSpeciality THEN @ExactlyPriorityOfSpeciality
+			WHEN @ExactlyIndexOfDisease THEN @ExactlyPriorityOfDisease
+
+			WHEN @RelativeIndexOfTag THEN
+				[dbo].[FU_TAKE_NUMBER_OF_RELATIVE_TAG] (@HospitalID, @WhatPhrase) * @RelativePriorityOfTag
+			WHEN @RelativeIndexOfSpeciality THEN
+				[dbo].[FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY] (@HospitalID, @WhatPhrase) * @RelativePriorityOfSpeciality
+			WHEN @RelativeIndexOfDisease THEN
+				[dbo].[FU_TAKE_NUMBER_OF_RELATIVE_DISEASE] (@HospitalID, @WhatPhrase) * @RelativePriorityOfDisease
+
+			WHEN @NonDiacriticIndexOfTag THEN
+				[dbo].[FU_TAKE_NUMBER_OF_RELATIVE_TAG] (@HospitalID, @WhatPhrase) * @NonDiacriticPriorityOfTag
+			WHEN @NonDiacriticIndexOfSpeciality
+				THEN [dbo].[FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY] (@HospitalID, @WhatPhrase) * @NonDiacriticPriorityOfSpeciality
+			WHEN @NonDiacriticIndexOfDisease
+				THEN [dbo].[FU_TAKE_NUMBER_OF_RELATIVE_DISEASE] (@HospitalID, @WhatPhrase) * @NonDiacriticPriorityOfDisease
+		END
+
+	-- RETURN SORT PRIORTY
+	RETURN @Priority
+END
