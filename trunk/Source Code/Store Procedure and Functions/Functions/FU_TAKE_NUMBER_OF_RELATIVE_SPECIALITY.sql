3e78c125db526327d7bb7b3aa710b59cb31f57e8
sonnx0907@gmail.com <sonnx0907@gmail.com@38b3c4e2-f945-9e19-cc8c-f996db3e81df>
2014-08-31 17:54:10 +0000

    Update stored procedure SP_NORMAL_SEARCH_HOSPITAL and its support functions

--- trunk/Source Code/Store Procedure and Functions/Functions/FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY.sql
+++ trunk/Source Code/Store Procedure and Functions/Functions/FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY.sql
@@ -6,7 +6,8 @@ GO
 CREATE FUNCTION [dbo].[FU_TAKE_NUMBER_OF_RELATIVE_SPECIALITY]
 (
 	@HospitalID INT,
-	@WhatPhrase NVARCHAR(4000)
+	@WhatPhrase NVARCHAR(4000),
+	@Mode INT
 )
 RETURNS INT
 AS
@@ -18,12 +19,28 @@ BEGIN
 
 	DECLARE @NumberOfSpeciality INT
 
-	SELECT @NumberOfSpeciality = (SELECT COUNT(s.Speciality_ID)
-								  FROM Speciality s, Hospital h, Hospital_Speciality hs
-								  WHERE FREETEXT (Speciality_Name, @WhatPhrase) AND
-										s.Speciality_ID = hs.Speciality_ID AND
-										h.Hospital_ID = hs.Hospital_ID AND
-										h.Hospital_ID = @HospitalID)
+	IF (@Mode = 0)
+	BEGIN
+		SELECT @NumberOfSpeciality = (SELECT COUNT(s.Speciality_ID)
+									  FROM Speciality s, Hospital h, Hospital_Speciality hs
+									  WHERE h.Hospital_ID = @HospitalID AND
+											h.Hospital_ID = hs.Hospital_ID AND
+											hs.Speciality_ID = s.Speciality_ID AND
+											FREETEXT (Speciality_Name, @WhatPhrase))
+	END
+	ELSE
+	BEGIN
+		SELECT @NumberOfSpeciality = (SELECT COUNT(s.Speciality_ID)
+									  FROM Speciality s, Hospital h, Hospital_Speciality hs
+									  WHERE h.Hospital_ID = @HospitalID AND
+											h.Hospital_ID = hs.Hospital_ID AND
+											hs.Speciality_ID = s.Speciality_ID AND
+											(N'%' + @WhatPhrase + N'%' LIKE 
+										     N'%' + [dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE](Speciality_Name) + N'%' OR
+										     N'%' + [dbo].[FU_TRANSFORM_TO_NON_DIACRITIC_VIETNAMESE](Speciality_Name) + N'%' LIKE
+										     N'%' + @WhatPhrase + N'%'))
+	END
+
 
 	IF (@NumberOfSpeciality IS NULL)
 		RETURN 0;
